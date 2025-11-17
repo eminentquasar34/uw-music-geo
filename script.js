4694eee3e3b89d5b6d3f288647a576f9b5f9278d
Daniel Zhang <72416560+eminentquasar34@users.noreply.github.com>
2025-11-17 15:58:54 -0800

    First sound uploaded

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-var map = L.map("map").setView([47.655, -122.303], 13);
+var map = L.map("map").setView([47.655, -122.303], 15);
 
 L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
   attribution:
     '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
 }).addTo(map);
 
-L.marker([47.655, -122.303])
+L.marker([47.655, -122.3])
   .addTo(map)
   .bindPopup(
     `A pretty CSS popup.<br> Easily customizable.<br>
@@ -13,4 +13,10 @@ L.marker([47.655, -122.303])
         <source src="audio/test.wav">
     </audio>`
   )
+  .openPopup(); // Remove this line when we want all popups to start on close
+
+// UW CS Allen Building
+L.marker([47.65332, -122.3055])
+  .addTo(map)
+  .bindPopup(`Studying in cs allen: Ceilings, by Lizzy Mcalpine`)
   .openPopup();
