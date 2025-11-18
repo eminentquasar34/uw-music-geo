8190c4a6b48c4b3b5d1ba6cd6e6368eeaa8767a9
Daniel Zhang <72416560+eminentquasar34@users.noreply.github.com>
2025-11-18 10:03:39 -0800

    Test m4a audio file

--- script.js
+++ script.js
@@ -8,15 +8,19 @@ L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
 L.marker([47.655, -122.3])
   .addTo(map)
   .bindPopup(
-    `A pretty CSS popup.<br> Easily customizable.<br>
+    `Test<br>
     <audio controls>
         <source src="audio/test.wav">
     </audio>`
-  )
-  .openPopup(); // Remove this line when we want all popups to start on close
+  );
 
 // UW CS Allen Building
 L.marker([47.65332, -122.3055])
   .addTo(map)
-  .bindPopup(`Studying in cs allen: Ceilings, by Lizzy Mcalpine`)
-  .openPopup();
+  .bindPopup(`Studying in cs allen: Ceilings, by Lizzy Mcalpine`);
+
+L.marker([47.66216, -122.30001]).addTo(map)
+  .bindPopup(`Music while walking around University Village!<br>Note: m4a file!<br>
+    <audio controls>
+        <source src="audio/uvill1.m4a">
+    </audio>`);
