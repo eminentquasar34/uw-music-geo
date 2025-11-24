cb5efc25eb737c57dc3835820f36a54db5619557
Daniel Zhang <72416560+eminentquasar34@users.noreply.github.com>
2025-11-24 14:17:34 -0800

    Made markers larger

--- script.js
+++ script.js
@@ -16,6 +16,7 @@ L.tileLayer("https://{s}.basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}.png", 
 }).addTo(map);
 
 L.circleMarker([47.66216, -122.30001], {
+  radius: 15,
   color: YELLOW,
 }).addTo(map).bindPopup(`
     <h3>Daniel Z: Walking in U-Village</h3>
@@ -30,6 +31,7 @@ L.circleMarker([47.66216, -122.30001], {
 // DZ Walking in Red Square - Gray
 L.circleMarker([47.65579, -122.30921], {
   color: GRAY,
+  radius: 15,
 }).addTo(map).bindPopup(`
     <h3>Daniel Z: Walking in Red Square</h3>
     There was a band playing in the distance at Red Square! I was walking to do my CCG #6 and the 
@@ -42,6 +44,7 @@ L.circleMarker([47.65579, -122.30921], {
 // JC Choir Recital Practice - Yellow
 L.circleMarker([47.65755, -122.30567], {
   color: YELLOW,
+  radius: 15,
 })
   .addTo(map)
   .bindPopup(
@@ -60,6 +63,7 @@ Singing and being surrounded by music made me feel happy. I love being immersed 
 // RWM Nirvana Music - Yellow
 L.circleMarker([47.66171, -122.30655], {
   color: YELLOW,
+  radius: 15,
 })
   .addTo(map)
   .bindPopup(
@@ -76,6 +80,7 @@ I rarely listen to house music, and it rarely brings me joy, but that night it p
 // JZ Amazon Truck
 L.circleMarker([47.65601, -122.31499], {
   color: GRAY,
+  radius: 15,
 }).addTo(map).bindPopup(`<h3>Jasmine Z: Amazon Truck</h3>
 I encountered an Amazon track blasting Bollywood music outside of Lander Hall while walking home. While loud and slightly jarring since it was late at night, the music highlighted the juxtaposition between my exhausted mental state and the delivery driver's energetic enthusiasm. The tempo and sheer volume of the music seemed to be the momentum that was powering the driver through his late-night route.    
 <br><br>
@@ -86,6 +91,7 @@ I encountered an Amazon track blasting Bollywood music outside of Lander Hall wh
 // JZ Happy Birthday
 L.circleMarker([47.65922, -122.30865], {
   color: YELLOW,
+  radius: 15,
 }).addTo(map).bindPopup(`<h3>Jasmine Z: Happy Birthday</h3>
 Before an Asian Business Student Association meeting in PACCAR Hall, officers spontaneously sang Happy Birthday to the president of the club and presented him with a cake. It was heartwarming to see how the club has transformed professional relationships into lasting friendships.
 <br><br>
@@ -96,6 +102,7 @@ Before an Asian Business Student Association meeting in PACCAR Hall, officers sp
 // JC Loud Music from Speaker
 L.circleMarker([47.66363, -122.31316], {
   color: GREEN,
+  radius: 15,
 }).addTo(map).bindPopup(`<h3>Jordan C: Loud Music From Speaker</h3>
 (Garbled sample taken from low-quality earbud microphone)
 <br><br>
@@ -108,6 +115,7 @@ I was walking home and heard this loud music blasting from the people in front o
 // JC Climbing Gym
 L.circleMarker([47.66153, -122.31294], {
   color: GRAY,
+  radius: 15,
 }).addTo(map).bindPopup(`<h3>Jordan C: Climbing Gym</h3>
 I took this sample when I was climbing at the climbing gym. When I'm climbing, I get very in-the-zone. I never really notice the music playing because of my focus. I wasn't paying any attention to the music and don't think it had any effect on me.      <br><br>
     <audio controls>
@@ -117,6 +125,7 @@ I took this sample when I was climbing at the climbing gym. When I'm climbing, I
 // JC Guitar for Mom
 L.circleMarker([47.66339, -122.31389], {
   color: YELLOW,
+  radius: 15,
 }).addTo(map).bindPopup(`<h3>Jordan C: Guitar for Mom</h3>
 This quarter, I purchased a guitar. I've been trying to learn Landslide by Fleetwood Mac. This audio clip is from one of my initial attempts at singing and playing at the same time. I sent it to my mom. Although learning an instrument can be difficult, playing guitar and singing in my room always feels peaceful and happy.
       <audio controls>
@@ -126,6 +135,7 @@ This quarter, I purchased a guitar. I've been trying to learn Landslide by Fleet
 // JC Ed Sheeran in Safeway
 L.circleMarker([47.66431, -122.31392], {
   color: RED,
+  radius: 15,
 }).addTo(map).bindPopup(`<h3>Jordan C: Ed Sheeran in Safeway</h3>
 (Garbled sample taken from low-quality earbud microphone)
 <br><br>
@@ -138,6 +148,7 @@ I heard Perfect by Ed Sheeran playing at the grocery store. I do not like this s
 // JZ Chimes in Red Square
 L.circleMarker([47.65587, -122.30977], {
   color: YELLOW,
+  radius: 15,
 }).addTo(map).bindPopup(`<h3>Jasmine Z: Chimes in Red Square</h3>
 While walking through Red Square on my way to Odegaard, I heard disembodied chimes ringing out into the evening. They sounded too clear to be coming from inside a building, but I couldn't see anyone playing them from outside either. While slightly eerie, the chimes were calming and reminded me of the spontaneity of life (especially in nodes of human activity).
 <br><br>
@@ -148,6 +159,7 @@ While walking through Red Square on my way to Odegaard, I heard disembodied chim
 // JZ Percussion Performance in Red Square
 L.circleMarker([47.65707, -122.30754], {
   color: BLUE,
+  radius: 15,
 }).addTo(map).bindPopup(`<h3>Jasmine Z: Percussion Performance in the Quad</h3>
 I saw this musical performance as I was walking through the Quad. There were 3-4 people on the lawn playing various types of hand percussion and bells while another person sang. I believe they were supporting a cause/organization/person, but I didn't pay much attention because I was in a rush. As I walked away, I was reminded of the many other things that I haven't been able to make time for in college, including playing music (especially because like the performers, I also play percussion).
 <br><br>
@@ -158,6 +170,7 @@ I saw this musical performance as I was walking through the Quad. There were 3-4
 // JZ Phantom of the Opera
 L.circleMarker([47.6557, -122.31722], {
   color: YELLOW,
+  radius: 15,
 }).addTo(map).bindPopup(`<h3>Jasmine Z: Phantom of the Opera</h3>
 I've always loved listening to musical theater music and the soundtrack from The Phantom of the Opera is my favorite by far. This clip is me listening to "The Point of No Return" while taking a break from studying. 
   <br><br>
@@ -168,6 +181,7 @@ I've always loved listening to musical theater music and the soundtrack from The
 // RWM Avole on the Ave
 L.circleMarker([47.66289, -122.31339], {
   color: PINK,
+  radius: 15,
 }).addTo(map).bindPopup(`<h3>Ruth WM: Avole on the Ave</h3>
 I was walking down the Ave zoned out, thinking about whatever my to do list, when I was drawn to music bouncing out of a nearby coffee shop. Upon entry, I saw a DJ booth nestled behind the counter next to the espresso machine. It was a total vibe, but I stuck out like a sore thumb.
     <br><br>
